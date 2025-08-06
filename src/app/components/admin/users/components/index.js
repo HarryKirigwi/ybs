83f8cdf32462badd06a8cc7ba304b101015803ce
Harrison Kirigwi <harrykigs2003@gmail.com>
2025-08-06 18:49:30 +0300

    Modified admin implementation

--- src/app/components/admin/users/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as UserStats } from './UserStats';
-export { default as UserCategories } from './UserCategories';
-export { default as UserFilters } from './UserFilters';
-export { default as UserHeader } from './UserHeader';
-export { default as BulkActions } from './BulkActions'; 
